294fafcf4ecf7c4879546526d54a3e26cf1cdd40
SFA01 <sarafranzinoandrade@hotmail.com>
2022-06-24 12:37:19 -0400

    Added an authorization steps 3bii and 3biii

--- COMP2084_MidTerm_NET5/Controllers/JobsController.cs
+++ COMP2084_MidTerm_NET5/Controllers/JobsController.cs
@@ -7,9 +7,12 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using COMP2084_MidTerm_NET5.Data;
 using COMP2084_MidTerm_NET5.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace COMP2084_MidTerm_NET5.Controllers
 {
+    //Here I am asking for an authorization step 3bii
+    [Authorize]
     public class JobsController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -22,6 +25,9 @@ namespace COMP2084_MidTerm_NET5.Controllers
         // GET: Jobs
 
         //Here I am ordering by sector for step 3bi
+
+        //Here I am allowing anyone to access the page step 3bii
+        [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
             var orderjobContext = _context.Job.OrderBy (j => j.Sector);
@@ -29,6 +35,10 @@ namespace COMP2084_MidTerm_NET5.Controllers
         }
 
         // GET: Jobs/Details/5
+
+        //Here I am allowing anyone to access the page step 3bii
+        [AllowAnonymous]
+
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
